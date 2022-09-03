15965b2b3833aab65c19103ff4cfa314306fb6e2
Aabiskar Pandey <pandeyaabiskar@gmail.com>
2022-09-03 13:09:43 +0545

    Redux

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
-import Todo from "./components/Todo";
-import TodoList from "./components/TodoList";
-import Login from "./components/Login";
-import { useState } from "react";
+import { Counter } from "./components/Counter";
 
 function App() {
-  const [todoList, setTodoList] = useState([]);
 
   return (
     <div>
-      <Login/>
-      {/* <Todo
-        title="What do you want to do today?"
-        buttonText="Add Todo"
-        todoList={todoList}
-        setTodoList={setTodoList}
-      />
-
-      <TodoList todoList={todoList} /> */}
+      <Counter/>
     </div>
   );
 }
